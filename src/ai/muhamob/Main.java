c39383a4937e2b339be6381da1856b78f884c333
muhamob <yusov.aa@phystech.edu>
2019-09-17 22:11:26 +0300

    Add function to create Trade with switch

--- src/ai/muhamob/Main.java
+++ src/ai/muhamob/Main.java
@@ -8,10 +8,13 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main (String[] args) throws IOException {
-//        FileInputStream stream = new FileInputStream("./src/ai/muhamob/test.txt");
-//        InputStreamReader streamReader = new InputStreamReader(stream);
-        InputStreamReader streamReader = new InputStreamReader(System.in);
-        Parser parser = new Parser();
+        // Either you want to use file or std.in streams comment proper lines
+        FileInputStream stream = new FileInputStream("./src/ai/muhamob/test.txt");
+        InputStreamReader streamReader = new InputStreamReader(stream);
+
+//        InputStreamReader streamReader = new InputStreamReader(System.in);
+
+        Parser parser = new Parser("switch");
 
         parser.readInput(streamReader);
     }
--- src/ai/muhamob/parser/Parser.java
+++ src/ai/muhamob/parser/Parser.java
@@ -1,13 +1,20 @@
 package ai.muhamob.parser;
 
-import ai.muhamob.core.Trade;
-import ai.muhamob.core.TradeType;
+import ai.muhamob.core.*;
 
 import java.io.*;
 
-import static ai.muhamob.core.TradeType.FX_SPOT;
-
 public class Parser {
+
+    private String tradeSearch;
+
+    public Parser(String tradeSearch) {
+        // trade search determines which method of matching string with trade class will be used
+        // if tradeSearch is switch, then switch construction will be used
+        // else if enum, then enum will be used
+        // default enum
+        this.tradeSearch = tradeSearch;
+    }
 
     public void readInput(InputStreamReader streamReader) throws IOException {
 //        InputStreamReader inputStream = new InputStreamReader(stream);
@@ -33,9 +40,43 @@ public class Parser {
         s = reader.readLine();
         price = Double.parseDouble(s.split("=")[1]);
 
-        TradeType enumTradeType = TradeType.valueOf(type);
-        Trade trade = enumTradeType.createTrade(price);
+        if (this.tradeSearch.equals("switch")) {
+            return createTradeSwitch(type, price);
+        } else if (this.tradeSearch.equals("enum")) {
+            return createTradeEnum(type, price);
+        } else {
+            return createTradeEnum(type, price);
+        }
+    }
+
+    private Trade createTradeSwitch(String type, double price) {
+        Trade trade;
+
+        switch (type) {
+            case "FX_SPOT":
+                trade = new FxSpot(price);
+                break;
+            case "BOND":
+                trade = new Bond(price);
+                break;
+            case "COMMODITY_SWAO":
+                trade = new CommoditySpot(price);
+                break;
+            case "IR_SWAP":
+                trade = new IrSwap(price);
+                break;
+            default:
+                trade = new FxSpot(price);
+                break;
+        }
 
         return trade;
     }
+
+    private Trade createTradeEnum(String type, double price) {
+        // This is the second way to get proper class
+        TradeType enumTradeType = TradeType.valueOf(type);
+        return enumTradeType.createTrade(price);
+    }
+
 }
