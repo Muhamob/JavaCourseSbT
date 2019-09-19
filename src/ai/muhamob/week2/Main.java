b270840e6cf77b4a577fd1ba9deea4006cfd9e90
muhamob <yusov.aa@phystech.edu>
2019-09-19 20:20:00 +0300

    Use command line to set either to get input from std.in or file

--- src/ai/muhamob/week2/Main.java
+++ src/ai/muhamob/week2/Main.java
@@ -1,20 +1,32 @@
 package ai.muhamob.week2;
 
 import ai.muhamob.week2.parser.Parser;
-import java.io.IOException;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+
+import java.io.*;
 
 public class Main {
+
+    private static String tradeSearchMethod = "enum";
+
     public static void main (String[] args) throws IOException {
         // Either you want to use file or std.in streams comment proper lines
-        FileInputStream stream = new FileInputStream("./src/ai/muhamob/week2/test.txt");
-        InputStreamReader streamReader = new InputStreamReader(stream);
+        InputStreamReader streamReader = getStreamReader(args);
 
-//        InputStreamReader streamReader = new InputStreamReader(System.in);
-
-        Parser parser = new Parser("switch");
-
+        Parser parser = new Parser(tradeSearchMethod);
         parser.readInput(streamReader);
+    }
+
+    private static InputStreamReader getStreamReader(String[] args) throws IOException {
+        // Return proper StreamReader depending on args
+
+        if (args.length == 0) {
+            return new InputStreamReader(System.in);
+        } else if (args.length == 1) {
+            FileInputStream stream = new FileInputStream(args[0]);
+            return new InputStreamReader(stream);
+        } else {
+            // this handles cases with incorrect arguments
+            return new InputStreamReader(System.in);
+        }
     }
 }
